5718903d5465915cf9ec5b6fe213eedde916de9f
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-02-20 15:36:06 +0300

    + pgsql

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -29,6 +29,7 @@ class Database
     protected $adapters = [
         'mysql'  => Adapter\MySQL::class,
         'sqlite' => Adapter\SQLite::class,
+        'pgsql'  => Adapter\PostgresSQL::class,
     ];
 
     /**
